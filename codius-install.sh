ec63e4c8d5023baf974908706464281206fc08d6
N3TC4T <netcat.av@gmail.com>
2018-06-09 05:01:43 +0430

    Update codius-install.sh

--- codius-install.sh
+++ codius-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# https://github.com/xrp-community/codius-installer
+# https://github.com/xrp-community/codius-install
 #
 # Copyright (c) 2018 XRP Community. Released under the MIT License.
 #
