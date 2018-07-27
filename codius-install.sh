dc6b2c512e0028f7adb0418686dd28b7afd00fa8
SneakyFish5 <32284796+SneakyFish5@users.noreply.github.com>
2018-07-27 14:28:20 -0500

    Also remove Yarn installer URL

--- codius-install.sh
+++ codius-install.sh
@@ -39,7 +39,6 @@ HYPERD_URL="https://codius-hyper-install.s3.amazonaws.com/hyper-bootstrap.sh"
 ########## Nodejs ##########
 NODEJS_RPM_URL="https://rpm.nodesource.com/setup_10.x"
 NODEJS_DEB_URL="https://deb.nodesource.com/setup_10.x"
-YARN_URL="https://yarnpkg.com/install.sh"
 ########## Certbot ##########
 CERTBOT_AUTO_URL="https://dl.eff.org/certbot-auto"
 ########## Constant ##########
