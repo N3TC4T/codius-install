f495ca8e0804f8dc3d7ed82e1fa5f75645e0debd
N3TC4T <netcat.av@gmail.com>
2018-08-08 00:34:36 +0430

    Fix #30

--- codius-install.sh
+++ codius-install.sh
@@ -613,11 +613,11 @@ server {
 
     location / {
       proxy_pass http://127.0.0.1:3000;
-      proxy_set_header Host \$host;
-      proxy_set_header X-Forwarded-For \$remote_addr;
       proxy_set_header Upgrade \$http_upgrade;
       proxy_set_header Connection \$connection_upgrade;
       proxy_buffering  off;
+      proxy_set_header Host \$host;
+      proxy_set_header X-Forwarded-For \$remote_addr;
       proxy_connect_timeout	  300;
       proxy_send_timeout   	  300;
       proxy_read_timeout          300;
